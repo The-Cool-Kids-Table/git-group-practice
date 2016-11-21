019afff24f7ea685f2ec1cfb6de7264ab7a608d5
Sabrina Mesa <msabrina88@gmail.com>
2016-11-21 11:55:29 -0500

    js ready

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('js connected');
+
+
+(() => {
+  const title= document.querySelector('h1');
+
+  const customizeTitle = () => {
+    title.style.color = 'orange';
+  })
+customizeTitle();
+
+});
